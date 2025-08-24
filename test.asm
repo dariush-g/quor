229835df0a1ac582564eeef15d6f2387a71882db
Dariush Ghassemieh <dariush@ghassemieh.com>
2025-08-24 13:06:32 -0700

    memory leak with vardecl from pointer vars

--- test.asm
+++ test.asm
@@ -13,6 +13,10 @@ main:
 push rbp
 mov rbp, rsp
 sub rsp, 0
+.while_start_0:
+mov rcx, 1
+cmp rcx, 1
+jne .while_end_0
 sub rsp, 8
 mov rdi, 7
 call malloc
@@ -36,24 +40,25 @@ mov byte [rax + 4], 'd'
 mov byte [rax + 5], 0
 mov r12, rax
 mov qword [rbp - 16], r12
-sub rsp, 8
-mov rcx, qword [rbp - 8]
-mov rdx, qword [rbp - 16]
-mov rdi, rcx
-mov rsi, rdx
-call concat
-mov qword [rbp - 24], rax
+mov rdx, qword [rbp - 8]
+mov rax, 1
+mov rdi, rdx
+mov rsi, rax
+call get_char_at
+mov rdi, rax
+call print_char
 mov r8, qword [rbp - 8]
 mov rdi, r8
 call free
 mov r9, qword [rbp - 16]
 mov rdi, r9
 call free
-mov r10, qword [rbp - 24]
-mov rdi, r10
-call print_str
-mov r11, 0
-mov rax, r11
+add rsp, 16
+jmp .while_start_0
+.while_end_0:
+mov r10, 0
+xor rax, rax
+mov rax, r10
 jmp .Lret_main
 .Lret_main:
 mov rsp, rbp
@@ -67,134 +72,17 @@ sub rsp, 16
 mov qword [rbp - 8], rdi
 mov dword [rbp - 12], esi
 sub rsp, 8
+mov r11, qword [rbp - 8]
 mov r13, qword [rbp - 8]
-mov rdi, r13
-call strlen
-mov qword [rbp - 24], rax
 mov r14d, dword [rbp - 12]
-mov r15d, dword [rbp - 24]
-cmp r14, r15
-setge al
-movzx rax, al
-cmp rax, 0
-je .else0
-mov rbx, 1
-mov rdi, rbx
-call exit
-.else0:
-sub rsp, 8
-mov r12, qword [rbp - 8]
-mov rcx, qword [rbp - 8]
-mov edx, dword [rbp - 12]
-add rcx, rdx
-mov qword [rbp - 32], rcx
-mov r8, qword [rbp - 32]
-sub rsp, 8
-mov qword [rbp - 40], r8
-mov r9, qword [rbp - 32]
-mov rdi, r9
-call free
-mov r10b, byte [rbp - 40]
-mov rax, r10
+add r13, r14
+mov qword [rbp - 24], r13
+mov r15, qword [rbp - 24]
+mov cl, byte [r15]
+xor rax, rax
+mov rax, rcx
 jmp .Lret_get_char_at
 .Lret_get_char_at:
-mov rsp, rbp
-pop rbp
-ret
-global concat
-concat:
-push rbp
-mov rbp, rsp
-sub rsp, 16
-mov qword [rbp - 8], rdi
-mov qword [rbp - 16], rsi
-sub rsp, 8
-mov r11, qword [rbp - 8]
-mov rdi, r11
-call strlen
-mov qword [rbp - 24], rax
-sub rsp, 8
-mov r13, qword [rbp - 16]
-mov rdi, r13
-call strlen
-mov qword [rbp - 32], rax
-sub rsp, 8
-sub rsp, 8
-mov r14, qword [rbp - 40]
-mov qword [rbp - 48], r14
-sub rsp, 8
-mov r15, 0
-mov qword [rbp - 56], r15
-.while_start_1:
-mov ecx, dword [rbp - 56]
-mov r8d, dword [rbp - 24]
-cmp rcx, r8
-setl al
-movzx rax, al
-cmp rax, 1
-jne .while_end_1
-sub rsp, 8
-mov r9, qword [rbp - 8]
-mov r11d, dword [rbp - 56]
-mov rdi, r9
-mov rsi, r11
-call get_char_at
-mov qword [rbp - 64], rax
-mov rax, qword [rbp - 40]
-mov r15b, byte [rbp - 64]
-mov byte [rax], r15b
-mov rcx, qword [rbp - 40]
-mov r8d, 1
-add rcx, r8
-mov qword [rbp - 40], rcx
-mov eax, dword [rbp - 56]
-mov r9, 1
-add rax, r9
-mov qword [rbp - 56], rax
-add rsp, 64
-jmp .while_start_1
-.while_end_1:
-sub rsp, 8
-mov rax, 0
-mov qword [rbp - 72], rax
-.while_start_2:
-mov eax, dword [rbp - 72]
-mov r15d, dword [rbp - 32]
-cmp rax, r15
-setl al
-movzx rax, al
-cmp rax, 1
-jne .while_end_2
-mov r8, qword [rbp - 16]
-mov ecx, dword [rbp - 56]
-mov rdi, r8
-mov rsi, rcx
-call get_char_at
-mov qword [rbp - 64], rax
-mov r9, qword [rbp - 40]
-mov al, byte [rbp - 64]
-mov byte [r9], al
-mov r15, qword [rbp - 40]
-mov r8d, 1
-add r15, r8
-mov qword [rbp - 40], r15
-mov ecx, dword [rbp - 72]
-mov rax, 1
-add rcx, rax
-mov qword [rbp - 72], rcx
-add rsp, 72
-jmp .while_start_2
-.while_end_2:
-mov r9, qword [rbp - 40]
-mov rax, 0
-mov byte [r9], al
-mov rdi, 1
-call malloc
-mov byte [rax + 0], 0
-mov r15, rax
-mov rax, r15
-jmp .Lret_concat
-.Lret_concat:
 mov rsp, rbp
 pop rbp
 ret
