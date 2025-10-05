4e88babe17a0d025f825f8f38b07f9164c59ccc9
Dariush Ghassemieh <dariush@ghassemieh.com>
2025-10-04 17:07:56 -0700

    started proper print function for io

--- pre/test.asm
+++ pre/test.asm
@@ -8,11 +8,8 @@ main:
 push rbp
 mov rbp, rsp
 sub rsp, 0
-sub rsp, 8
 mov rdi, 6
-sub rsp, 8
 call malloc
-add rsp, 8
 mov byte [rax + 0], 'h'
 mov byte [rax + 1], 'e'
 mov byte [rax + 2], 'l'
@@ -20,38 +17,15 @@ mov byte [rax + 3], 'l'
 mov byte [rax + 4], 'o'
 mov byte [rax + 5], 0
 mov rbx, rax
-mov qword [rbp - 8], rbx
-xor rcx, rcx
-mov rcx, qword [rbp - 8]
-mov rdx, 1
-mov r8, 3
-mov rdi, rcx
-mov rsi, rdx
-mov rdx, r8
-sub rsp, 8
-call substring
-add rsp, 8
-mov r9, rax
-mov rdi, r9
-sub rsp, 8
-call print_str
-add rsp, 8
-mov r10, rax
-xor r11, r11
-mov r11, qword [rbp - 8]
-mov rdi, r11
-sub rsp, 8
-call print_str
-add rsp, 8
-mov r12, rax
-mov rdi, 10
-sub rsp, 8
-call print_char
-add rsp, 8
 mov rdi, rbx
-mov r13, 0
+call print
+mov rcx, rax
+mov rdi, 10
+call print_char
+mov rdi, rbx
+mov rdx, 0
 xor rax, rax
-mov rax, r13
+mov rax, rdx
 jmp .Lret_main
 .Lret_main:
 mov rsp, rbp
@@ -65,23 +39,23 @@ sub rsp, 16
 mov qword [rbp - 8], rdi
 mov dword [rbp - 12], esi
 sub rsp, 8
-xor r14, r14
-mov r14, qword [rbp - 8]
-xor r15, r15
-mov r15, qword [rbp - 8]
+xor r8, r8
+mov r8, qword [rbp - 8]
+xor r9, r9
+mov r9, qword [rbp - 8]
 sub rsp, 8
-mov qword [rsp], r15
-xor rbx, rbx
-mov ebx, dword [rbp - 12]
-mov rcx, qword [rsp]
+mov qword [rsp], r9
+xor r10, r10
+mov r10d, dword [rbp - 12]
+mov r11, qword [rsp]
 add rsp, 8
-add ecx, ebx
-mov qword [rbp - 24], rcx
-xor rdx, rdx
-mov rdx, qword [rbp - 24]
-mov r8b, byte [rdx]
+add r11d, r10d
+mov qword [rbp - 24], r11
+xor r12, r12
+mov r12, qword [rbp - 24]
+mov r13b, byte [r12]
 xor rax, rax
-mov rax, r8
+mov rax, r13
 jmp .Lret_char_at
 .Lret_char_at:
 mov rsp, rbp
@@ -93,199 +67,97 @@ push rbp
 mov rbp, rsp
 sub rsp, 16
 mov byte [rbp - 1], dil
-xor r9, r9
-mov r9b, byte [rbp - 1]
+xor r14, r14
+mov r14b, byte [rbp - 1]
 sub rsp, 8
-mov qword [rsp], r9
-mov r10, 'a'
-mov r11, qword [rsp]
-add rsp, 8
-cmp r11, r10
-setge al
-movzx rax, al
-mov r12, rax
-sub rsp, 8
-mov qword [rsp], r12
-xor r13, r13
-mov r13b, byte [rbp - 1]
-sub rsp, 8
-mov qword [rsp], r13
-mov r15, 'z'
+mov qword [rsp], r14
+mov r15, 'a'
 mov rbx, qword [rsp]
 add rsp, 8
 cmp rbx, r15
-setle al
+setge al
 movzx rax, al
 mov rcx, rax
-mov rdx, qword [rsp]
+sub rsp, 8
+mov qword [rsp], rcx
+xor rdx, rdx
+mov dl, byte [rbp - 1]
+sub rsp, 8
+mov qword [rsp], rdx
+mov r9, 'z'
+mov r10, qword [rsp]
 add rsp, 8
-cmp rdx, 1
+cmp r10, r9
+setle al
+movzx rax, al
+mov r11, rax
+mov r12, qword [rsp]
+add rsp, 8
+cmp r12, 1
 jne .and_end_0
-cmp rcx, 1
+cmp r11, 1
 jne .and_end_0
 mov rax, 1
 jmp .and_done_1
 .and_end_0:
 mov rax, 0
 .and_done_1:
-mov r8, rax
-cmp r8, 0
+mov r13, rax
+cmp r13, 0
 je .else1
-mov r9, 1
+mov r14, 1
 xor rax, rax
-mov rax, r9
+mov rax, r14
 jmp .Lret_is_alphabetic
 .else1:
-xor r11, r11
-mov r11b, byte [rbp - 1]
-sub rsp, 8
-mov qword [rsp], r11
-mov r10, 'A'
-mov r12, qword [rsp]
-add rsp, 8
-cmp r12, r10
-setge al
-movzx rax, al
-mov r13, rax
-sub rsp, 8
-mov qword [rsp], r13
 xor rbx, rbx
 mov bl, byte [rbp - 1]
 sub rsp, 8
 mov qword [rsp], rbx
-mov r15, 'Z'
-mov rdx, qword [rsp]
+mov r15, 'A'
+mov rcx, qword [rsp]
 add rsp, 8
-cmp rdx, r15
+cmp rcx, r15
+setge al
+movzx rax, al
+mov rdx, rax
+sub rsp, 8
+mov qword [rsp], rdx
+xor r10, r10
+mov r10b, byte [rbp - 1]
+sub rsp, 8
+mov qword [rsp], r10
+mov r9, 'Z'
+mov r12, qword [rsp]
+add rsp, 8
+cmp r12, r9
 setle al
 movzx rax, al
-mov rcx, rax
-mov r8, qword [rsp]
+mov r11, rax
+mov r13, qword [rsp]
 add rsp, 8
-cmp r8, 1
+cmp r13, 1
 jne .and_end_2
-cmp rcx, 1
+cmp r11, 1
 jne .and_end_2
 mov rax, 1
 jmp .and_done_3
 .and_end_2:
 mov rax, 0
 .and_done_3:
-mov r9, rax
-cmp r9, 0
+mov r14, rax
+cmp r14, 0
 je .else3
-mov r11, 1
+mov rbx, 1
 xor rax, rax
-mov rax, r11
+mov rax, rbx
 jmp .Lret_is_alphabetic
 .else3:
-mov r12, 0
+mov rcx, 0
 xor rax, rax
-mov rax, r12
+mov rax, rcx
 jmp .Lret_is_alphabetic
 .Lret_is_alphabetic:
-mov rsp, rbp
-pop rbp
-ret
-global substring
-substring:
-push rbp
-mov rbp, rsp
-sub rsp, 16
-mov qword [rbp - 8], rdi
-mov dword [rbp - 12], esi
-mov dword [rbp - 16], edx
-xor r10, r10
-mov r10d, dword [rbp - 16]
-sub rsp, 8
-mov qword [rsp], r10
-xor r13, r13
-mov r13d, dword [rbp - 12]
-mov rbx, qword [rsp]
-add rsp, 8
-sub ebx, r13d
-sub rsp, 8
-mov qword [rsp], rbx
-mov rdx, 1
-mov r15, qword [rsp]
-add rsp, 8
-add r15d, edx
-mov rdi, r15
-call malloc
-mov r8, rax
-sub rsp, 8
-mov qword [rbp - 24], r8
-sub rsp, 8
-xor rcx, rcx
-mov rcx, qword [rbp - 24]
-mov qword [rbp - 32], rcx
-xor r9, r9
-mov r9, qword [rbp - 8]
-xor r11, r11
-mov r11, qword [rbp - 8]
-sub rsp, 8
-mov qword [rsp], r11
-xor r12, r12
-mov r12d, dword [rbp - 12]
-mov r10, qword [rsp]
-add rsp, 8
-add r10d, r12d
-mov qword [rbp - 8], r10
-sub rsp, 8
-mov r13, 0
-mov dword [rbp - 40], r13d
-.while_start_4:
-xor rbx, rbx
-mov ebx, dword [rbp - 40]
-sub rsp, 8
-mov qword [rsp], rbx
-xor rdx, rdx
-mov edx, dword [rbp - 16]
-sub rsp, 8
-mov qword [rsp], rdx
-xor r15, r15
-mov r15d, dword [rbp - 12]
-mov r8, qword [rsp]
-add rsp, 8
-sub r8d, r15d
-mov rcx, qword [rsp]
-add rsp, 8
-cmp rcx, r8
-setl al
-movzx rax, al
-mov r11, rax
-cmp r11, 1
-jne .while_end_4
-xor r12, r12
-mov r12, qword [rbp - 24]
-xor r10, r10
-mov r10, qword [rbp - 8]
-mov r13b, byte [r10]
-mov byte [r12], r13b
-xor rbx, rbx
-mov rbx, qword [rbp - 24]
-mov edx, 1
-add rbx, rdx
-mov qword [rbp - 24], rbx
-xor r15, r15
-mov r15, qword [rbp - 8]
-mov ecx, 1
-add r15, rcx
-mov qword [rbp - 8], r15
-mov r8d, dword [rbp - 40]
-inc dword [rbp - 40]
-jmp .while_start_4
-.while_end_4:
-xor r11, r11
-mov r11, qword [rbp - 24]
-mov r10, 0
-mov byte [r11], r10b
-xor r12, r12
-mov r12, qword [rbp - 32]
-xor rax, rax
-mov rax, r12
-jmp .Lret_substring
-.Lret_substring:
 mov rsp, rbp
 pop rbp
 ret
