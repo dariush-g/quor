f4f676a7974d64d546326ddda25d3a0d4d7117e5
dariushg-g <dghassemieh2@hwemail.com>
2026-01-05 14:38:05 -0800

    ir

--- pre/src/ir/expr.rs
+++ pre/src/ir/expr.rs
@@ -14,9 +14,9 @@ impl IRGenerator {
         expr: Expr,
         ir_program: &mut IRProgram,
         var_map: &HashMap<String, (Option<usize>, Option<VReg>)>,
-        var_count: usize,
-    ) -> IRInstruction {
-        let (value, ty) = match expr {
+        var_count: &mut usize,
+    ) -> (Value, Type) {
+        match expr {
             Expr::IntLiteral(i) => (Value::Const(i as i64), Type::int),
             Expr::LongLiteral(l) => (Value::Const(l), Type::Long),
             Expr::FloatLiteral(f) => (Value::ConstFloat(f as f64), Type::float),
@@ -24,12 +24,15 @@ impl IRGenerator {
             Expr::StringLiteral(s) => {
                 todo!()
             }
-            Expr::CharLiteral(_) => todo!(),
+            Expr::CharLiteral(c) => (Value::Const(c as i64), Type::Char),
             Expr::StructInit { name, params } => todo!(),
             Expr::AddressOf(expr) => todo!(),
             Expr::DerefAssign { target, value } => todo!(),
             Expr::InstanceVar(_, _) => todo!(),
-            Expr::Variable(_, _) => todo!(),
+            Expr::Variable(name, ty) => {
+                let addr = var_map.get(&name).unwrap().0.unwrap();
+                (Value::Local(addr), ty)
+            }
             Expr::Assign { name, value } => todo!(),
             Expr::CompoundAssign { name, op, value } => todo!(),
             Expr::PreIncrement { name } => todo!(),
@@ -65,16 +68,6 @@ impl IRGenerator {
                 field,
                 value,
             } => todo!(),
-        };
-
-        let count = var_count;
-        var_count += 1;
-
-        IRInstruction::Store {
-            value,
-            addr: Value::Local(count),
-            offset: 0,
-            ty,
         }
     }
 }
